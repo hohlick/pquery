7772f7fd19c503a5354951fbb49b7d989d16a4ea
Maxim Zelensky <zelensky@yandex.ru>
2017-08-10 15:45:41 +0300

    Pasted tycho's changes
    cannot auto merge

--- Table.CrossJoinTables.m
+++ Table.CrossJoinTables.m
@@ -1,32 +1,33 @@
 /*
 // Cross-join a list of tables into one super table containing every combination of rows of its constituents
 // Usage:
-    Table.CrossJoinTables = Load("Table.CrossJoinTables"),
-    Table.CrossJoinTables({
-      #table({"A","B"},{{"A",1},{"B",2}}),
-      #table({"C","D"},{{"E",3},{"F",4}})
-    })
+  Table.CrossJoinTables = Load("Table.CrossJoinTables"),
+  Table.CrossJoinTables({
+    #table({"A","B"},{{"A",1},{"B",2}}),
+    #table({"C","D"},{{"E",3},{"F",4}})
+  })
 // Result: a cross-joined version of all the tables. beware of performance, the result could get big!
-// Revision: Maxim Zelensky Aug 09, 2017
-// Revision purpose: use Table.Join instead of Table.ExpandTableColumn (used in referenced function) to get performance gain
 */
 
-(tables as list) as table =>
-    let 
-        CrossJoin = List.Accumulate(
-            List.Positions(tables), 
-            #table({},{}), 
-            (state, current) => 
-                let
-                    CurrentTable = tables{current}, // need check for table type?
-                    CurrentNames = Table.ColumnNames(state),
-                    OldNames = Table.ColumnNames(CurrentTable),
-                    NewNames = List.Transform(OldNames, each if List.Contains(CurrentNames, _) then Text.From(current+1) & "_" & _ else _),
-                    Renamed = Table.RenameColumns(CurrentTable, List.Zip({OldNames, NewNames}))
-                in
-                    Table.Join(state, {}, Renamed, {}, JoinKind.FullOuter)
-            )
+(
+  tables as list,
+  optional TableNames as list, // names to append as prefixes on clash or if desired, default {1, 2, 3, ...}
+  optional AlwaysPrefix as logical    //whether to use append table names if without column name clashes
+) as table => let
+  CrossJoin = List.Accumulate(
+    List.Positions(tables),
+    #table({},{}), 
+    (state, current) => let
+      CurrentTable = tables{current}, // need check for table type?
+      CurrentNames = Table.ColumnNames(state),
+      OldNames = Table.ColumnNames(CurrentTable),
+      Prefix = if TableNames <> null then try TableNames{current} otherwise Text.From(current+1) else Text.From(current+1),
+      NewNames = List.Transform(OldNames, each if AlwaysPrefix or List.Contains(CurrentNames, _) then Prefix & "_" & _ else _),
+      Renamed = Table.RenameColumns(CurrentTable, List.Zip({OldNames, NewNames}))
     in
-        if List.IsEmpty(tables) 
-            then #table({},{}) 
-            else CrossJoin
+      Table.Join(state, {}, Renamed, {}, JoinKind.FullOuter)
+    )
+  in
+    if List.IsEmpty(tables) 
+      then #table({},{}) 
+      else CrossJoin
